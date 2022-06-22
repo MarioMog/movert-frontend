27418af7cce88bd4f328be75f8ebe3b28e12f5f0
MarioMog <mamg3108@gmail.com>
2022-06-22 09:09:37 -0500

    feat: change loged to see links and logout button

--- movert-employee/src/components/Navbar/index.jsx
+++ movert-employee/src/components/Navbar/index.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 
 import './style.css'
 const Navbar = () => {
-  const [logged] = useState(false)
+  const [logged] = useState(true)
   return (
     <>
       <nav>
