261811fb696e2518775f515dc24d5f9dabeee2c0
MarioMog <mamg3108@gmail.com>
2022-06-22 09:08:09 -0500

    feat: footer component

---
+++ movert-employee/src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './style.css'
+const Footer = () => {
+  return (
+    <footer>
+      <p>MarioMog 2022 - Contact me on <a href='#' target='blank'>{'mamg3108@gmail.com'}</a> for support</p>
+    </footer>
+  )
+}
+
+export default Footer
